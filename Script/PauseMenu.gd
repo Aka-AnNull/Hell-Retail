c9857bb2ef76cbb62edaa82af99f6956add68e49
Ann <dinoko2007@gmail.com>
2025-12-14 12:19:24 +0700

    Setting and Pause

---
+++ Script/PauseMenu.gd
@@ -0,0 +1,57 @@
+extends CanvasLayer
+
+@onready var settings_menu = $SettingsMenu
+@onready var content = $CenterContainer
+
+func _ready():
+	visible = false
+	settings_menu.visible = false
+	
+	# 1. CONNECT THE SIGNAL
+	# When settings says "menu_closed", run "_on_settings_closed"
+	if not settings_menu.menu_closed.is_connected(_on_settings_closed):
+		settings_menu.menu_closed.connect(_on_settings_closed)
+
+func _on_settings_closed():
+	# The settings just closed, so show the Pause Buttons again!
+	content.visible = true
+	
+func _input(event):
+	if event.is_action_pressed("ui_cancel"): # ESC Key
+		toggle_pause()
+
+func toggle_pause():
+	# 1. Close Settings if open
+	if settings_menu.visible:
+		settings_menu.close_settings()
+		content.visible = true
+		return
+
+	# 2. Toggle Pause
+	visible = not visible
+	get_tree().paused = visible
+	
+	if visible:
+		content.visible = true
+
+# --- BUTTONS ---
+# Make sure you connected the TextureButton signals to these!
+
+func _on_resume_button_pressed():
+	toggle_pause()
+
+func _on_restart_button_pressed():
+	toggle_pause() # Unpause first!
+	GameManager.current_hp = GameManager.max_hp
+	if GameManager.current_level == 8:
+		get_tree().change_scene_to_file("res://Scene/BossFight.tscn")
+	else:
+		get_tree().change_scene_to_file("res://Scene/level.tscn")
+
+func _on_settings_button_pressed():
+	content.visible = false # Hide buttons so they don't block the settings
+	settings_menu.open_settings()
+
+func _on_quit_button_pressed():
+	toggle_pause()
+	get_tree().change_scene_to_file("res://Scene/main_menu.tscn")
--- Script/main_menu.gd
+++ Script/main_menu.gd
@@ -1,14 +1,25 @@
 extends Node2D
 
 var button_type = null
+
+# --- NODES ---
 @onready var ske_menu = $"Skeleton Menu"
 @onready var hover_sfx = $HoverSFX
 @onready var click_sfx = $ClickSFX
+
+# Add this line! Make sure the node name matches your scene tree
+@onready var settings_menu = $SettingsMenu 
 
 func _ready():
 	$Fade_transition.show()
 	$Fade_transition/AnimationPlayer.play("Fade_out")
 	ske_menu.play("Skeleton Idle")
+	
+	# Make sure settings are hidden at start
+	if settings_menu:
+		settings_menu.visible = false
+
+# --- BUTTONS ---
 
 func _on_start_pressed():
 	click_sfx.play()
@@ -19,28 +30,29 @@ func _on_start_pressed():
 
 func _on_setting_pressed():
 	click_sfx.play()
-	button_type = "setting"
-	$Fade_transition.show()
-	$Fade_transition/Fade_timer.start()
-	$Fade_transition/AnimationPlayer.play("Fade_in")
+	# NO FADE NEEDED. Just open the popup directly.
+	settings_menu.open_settings()
 
 func _on_quit_pressed():
 	click_sfx.play()
 	await click_sfx.finished
+	# We can keep the fade for quitting if you like the dramatic exit
+	$Fade_transition.show() 
 	$Fade_transition/AnimationPlayer.play("Fade_in")
 	await $Fade_transition/AnimationPlayer.animation_finished
 	get_tree().quit()
+
+# --- TIMER (Only for Scene Changes) ---
 
 func _on_fade_timer_timeout():
 	print("TIMER FINISHED! ATTEMPTING TO SWITCH SCENE...")
 	
 	if button_type == "start":
-		# Just call the manager! It handles the rest.
 		GameManager.start_game()
-		
-	elif button_type == "setting":
-		pass
+	
+	# We removed "setting" from here because it doesn't use the timer anymore.
 
+# --- HOVER SFX ---
 func _on_start_mouse_entered() -> void:
 	hover_sfx.play()
 func _on_setting_mouse_entered() -> void:
